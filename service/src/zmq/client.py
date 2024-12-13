06a3f531575b3ba7406d548dad0fe0a41f43ba1b
Andre Merzky <andre@merzky.net>
2024-12-13 01:06:24 +0100

    snap

--- service/src/zmq/client.py
+++ service/src/zmq/client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 import radical.utils as ru
 
 
@@ -7,9 +9,17 @@ import radical.utils as ru
 #
 if __name__ == '__main__':
 
-    c = ru.zmq.Client(url='tcp://localhost:10000')
+    addr  = sys.argv[1]
+    fname = sys.argv[2]
+
+    with open(fname) as fin:
+        data = fin.read()
+
+    c = ru.zmq.Client(url=addr)
 
     uid = c.request('register')
+    result = c.request('register_data', uid=uid, data=data)
+
     print(1, c.request(cmd='register_data', uid=uid, urls=['http://example.com']))
 
 
